a63a12a1b54d317af5fc1a287bc217206aa4ce04
Jessica <duellje@oregonstate.edu>
2020-09-16 12:09:40 -0400

    completed single number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,23 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-
-    pass
+    #dictionary to keep track of numbers visited in array
+    visited = {}
+    #loop through the array 
+    for item in arr:
+        #check if the number is in the dictionary already
+        if item in visited:
+            #if already in dictionary add to the count
+            visited[item] += 1
+        else:
+            #if not in diciontary add it to the dictionary with the value of 1
+            visited[item] = 1
+            
+    #looks for the count of 1 in the dictionary
+    for key, value in visited.items():
+        if value == 1:
+            return key
+    
 
 
 if __name__ == '__main__':
