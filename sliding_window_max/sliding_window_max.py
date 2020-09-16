3b91e0552946722c59d517b6d2f392c2f510c5f8
Jessica <duellje@oregonstate.edu>
2020-09-16 13:22:19 -0400

    completd sliding window max

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -1,11 +1,28 @@
 '''
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
-'''
-def sliding_window_max(nums, k):
-    # Your code here
 
-    pass
+Given an array of integers, there is a sliding window of size k which is moving from the left side of the array to the right, one element at a time. You can only interact with the k numbers in the window. Return an array consisting of the maximum value of each window of elements.
+'''
+def sliding_window_max(arr, k):
+    #initialize empty array to hold max values
+    maxValues = []
+    #loop through the array
+    for index in range(len(arr)-1):
+        #check if the index plus k will go beyond the length of the array
+        if index + k > len(arr):
+            return maxValues
+        #reset max value
+        max = arr[index]
+        i = index
+        #loop through the window
+        while i < index + k and i <= len(arr)-1:
+            #check if the number in the array is greater than the max value
+            if max < arr[i]:
+                max = arr[i]
+            i += 1
+        maxValues.append(max)
+    return maxValues
 
 
 if __name__ == '__main__':
